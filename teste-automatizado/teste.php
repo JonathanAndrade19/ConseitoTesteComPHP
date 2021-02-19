60199800317e176458b7dffd333bd944bca4367d
Jonathan <jonathanjampamb@gmail.com>
2021-02-19 18:39:19 -0300

    new teste

--- teste-automatizado/teste.php
+++ teste-automatizado/teste.php
@@ -2,10 +2,23 @@
 
 require_once 'EscritorDeArquivo.php';
 
-$caminhoArquivo = 'Arquivo';
+// Definir o cenário
+$caminhoArquivo = md5(time());
 $escritor = new EscritorDeArquivo($caminhoArquivo);
 
+// Execultar a acão a ser testada
 $escritor->escreve('Primeira Linha');
 $escritor->escreve('Segunda Linha');
+
+// Verificar se o resultado e o esperado
+$conteudoEsperado = 'Primeira Linha
+Segunda Linha
+';
+
+if ($conteudoEsperado === file_get_contents($caminhoArquivo)){
+    echo 'TESTE OK'. PHP_EOL;
+} else {
+    echo 'TESTE FAILED'. PHP_EOL;
+}
 
 ?>
