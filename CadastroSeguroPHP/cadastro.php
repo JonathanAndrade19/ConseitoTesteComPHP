b6c7fe7edc1263c8070fb91722877497a6b9c1f3
Jonathan <jonathanjampamb@gmail.com>
2021-02-21 15:02:31 -0300

    new select in database

--- CadastroSeguroPHP/cadastro.php
+++ CadastroSeguroPHP/cadastro.php
@@ -1,5 +1,11 @@
 <?php
 
+include_once 'conexao.php';
 
+$listaCadastro = "SELECT * FROM cadastro";
+
+$result = mysqli_query($conn, $listaCadastro);
+
+$rows = mysqli_fetch_assoc($result);
 
 ?>
---
+++ CadastroSeguroPHP/conexao.php
@@ -0,0 +1,10 @@
+<?php
+
+$servidor = "localhost";
+$user  = "root";
+$pass = "acade123";
+$dbName = "heyjessy";
+
+$conn = mysqli_connect($servidor, $user, $pass, $dbName);
+
+?>
